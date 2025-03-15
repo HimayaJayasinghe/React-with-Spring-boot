104610e34065abe79c928d0a66df629a0f94e9f9
HimayaJayasinghe <hbinushi@gmail.com>
2025-03-15 14:38:47 +0530

    delete update done

--- src/main/java/net/javaguides/ems/controller/EmployeeController.java
+++ src/main/java/net/javaguides/ems/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package net.javaguides.ems.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems.dto.EmployeeDto;
+
 import net.javaguides.ems.services.EmployeeService;
 
 @RestController
@@ -13,6 +16,7 @@ import net.javaguides.ems.services.EmployeeService;
 public class EmployeeController {
 
     private final EmployeeService employeeService;
+
 
     //build add employee REST API
     @PostMapping
@@ -26,5 +30,30 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) {
         EmployeeDto employeeDto = employeeService.getEmployeeeById(employeeId);
         return ResponseEntity.ok(employeeDto);
+    }
+
+    //build get all employee rest Api
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getAllEmployee() {
+       List<EmployeeDto> employee = employeeService.getAllEmployees();
+       return ResponseEntity.ok(employee);
+    }
+
+
+    //build update rest Api
+
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeDto>updateEmployee(@PathVariable ("id")Long employeeId,
+    @RequestBody EmployeeDto updatedEmployee){
+       EmployeeDto employeeDto = employeeService.updateEmployee(employeeId,updatedEmployee);
+         return ResponseEntity.ok(employeeDto);
+        
+    }
+
+    //build delete rest Api
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId) {
+        employeeService.deleteEmployee(employeeId);
+        return ResponseEntity.ok("Employee deleted successfully");
     }
 }
--- src/main/java/net/javaguides/ems/services/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems/services/impl/EmployeeServiceImpl.java
@@ -2,6 +2,9 @@ package net.javaguides.ems.services.impl;
 
 import net.javaguides.ems.services.EmployeeService;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 import lombok.AllArgsConstructor;
@@ -37,6 +40,37 @@ public class EmployeeServiceImpl implements EmployeeService{
         new ResourceNotFoundException("Employee is not found with given id: "+employeeId));
         return EmployeeMapper.mapToEmployeeDto(employee);
         
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee)).collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+      Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee is not found with given id: "+employeeId));
+
+
+
+            employee.setFirstName(updatedEmployee.getFirstName());
+            employee.setLastName(updatedEmployee.getLastName());
+            employee.setEmail(updatedEmployee.getEmail());
+
+
+    Employee updatedEmployeeObj =   employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee is not found with given id: "+employeeId));
+     
+
+        employeeRepository.deleteById(employeeId);
     }
 
 
